09d7061766d7da3722666d24e1e2cfdf5ef96e5c
Elfkiri Imad <elfkiriimad@gmail.com>
2025-08-03 11:53:34 +0000

    Améliore l’en-tête de la page Résultats :
    - Sur une seule ligne :
       -

--- src/app/resultats/page.tsx
+++ src/app/resultats/page.tsx
@@ -1,19 +1,13 @@
 
-import Link from 'next/link';
-import { Button } from "@/components/ui/button";
 import { ResultsTable } from "@/components/results-table";
 import {
   Card,
   CardContent,
 } from "@/components/ui/card";
-import { Plus } from 'lucide-react';
 
 export default function ResultsPage() {
   return (
-    <div className="flex flex-1 flex-col gap-6 p-4 lg:p-6 h-full">
-      <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold tracking-tight">Historique des Résultats</h1>
-      </div>
+    <div className="flex flex-1 flex-col gap-4 p-4 lg:p-6 h-full">
        <Card className="flex-1">
         <CardContent className="p-0 h-full">
             <ResultsTable />
