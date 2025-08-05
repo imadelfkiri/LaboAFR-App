f051aa8263cedc485dcf9d3043fcb4d48f5c56d0
Elfkiri Imad <elfkiriimad@gmail.com>
2025-08-05 16:02:08 +0000

    merci de changer le titre Spécifications AFR par Spécifications des AF

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -20,7 +20,7 @@ const pageTitles: { [key: string]: string } = {
   '/calculateur': 'Calculateur PCI',
   '/resultats': 'Historique des Résultats',
   '/statistiques': 'Tableau de Bord des Statistiques',
-  '/specifications': 'Spécifications AFR',
+  '/specifications': 'Spécifications des AF',
 };
 
 export function MainLayout({ children }: { children: React.ReactNode }) {
