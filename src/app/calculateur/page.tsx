173bad84d2f19974025ac721ea9885f76ce16c3e
Elfkiri Imad <elfkiriimad@gmail.com>
2025-08-08 22:37:05 +0000

    ?

--- src/app/calculateur/page.tsx
+++ src/app/calculateur/page.tsx
@@ -1,8 +1,9 @@
+
 import { PciCalculator } from '@/components/pci-calculator';
 
 export default function CalculatorPage() {
   return (
-    <div className="flex flex-1 items-start justify-center bg-gray-50 p-4 sm:p-6 lg:p-8">
+    <div className="flex flex-col flex-1 items-center justify-start bg-gray-50 p-4 sm:p-6 lg:p-8 w-full">
       <PciCalculator />
     </div>
   );
