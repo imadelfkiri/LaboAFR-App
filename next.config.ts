435461a6a0e2ccb1c221f430ce7e1a2591d5c1f7
Elfkiri Imad <elfkiriimad@gmail.com>
2025-10-22 08:31:32 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+    serverComponentsExternalPackages: [],
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
